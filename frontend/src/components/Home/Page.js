4b19644536931a68f74a836ccdf1ce55e7594647
tayyabRazzaq <tayyab.razzaq401@gmail.com>
2019-02-05 00:03:52 +0500

    feat: List Views Added
    basic categories display in sidebar
    basic list view based on categories added

--- frontend/src/components/Home/Page.js
+++ frontend/src/components/Home/Page.js
@@ -1,34 +1,146 @@
-import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import React, {Component} from 'react';
 import * as PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import MenuIcon from '@material-ui/icons/Menu';
 import homeStyle from '../../styles/homeStyles';
+import {getCategories, getAllPosts, getCategoryPosts} from '../../utils/_DATA';
 
-const ButtonAppBar = props => {
-    const {classes} = props;
-    return (
-        <div className={classes.root}>
-            <AppBar
-                position="static" color='default'
-                classes={{
-                    colorDefault: classes.appBar
-                }}>
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" className={classes.grow}>
-                        Readable
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
-};
+
+class ButtonAppBar extends Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            categories: [],
+            posts: [],
+            showSideBar: true
+        };
+    }
+    
+    componentDidMount() {
+        getCategories().then(response => {
+            debugger; // eslint-disable-line
+            this.setState({categories: response.data.categories});
+        });
+        getAllPosts().then(response => {
+            debugger; // eslint-disable-line
+            this.setState({posts: response.data});
+        });
+    }
+    
+    toggleSideBar = () => this.setState(prevState => ({showSideBar: !prevState.showSideBar}));
+    
+    handleNavClick = key => {
+        getCategoryPosts(key).then(response => {
+            debugger; // eslint-disable-line
+            this.setState({posts: response.data}, () => {
+                const {history} = this.props;
+                history.push(key);
+            });
+        });
+    };
+    
+    render()    {
+        const {classes, history} = this.props;
+        const {posts, categories, showSideBar} = this.state;
+    
+        const navBarStyling = [classes.sideNav];
+        if (showSideBar) {
+            navBarStyling.push(classes.sideNavToggle);
+        }
+        
+        const categoriesList = categories.map(category => {
+            const navItemClasses = [classes.itemGroup];
+            if (`/${category.path}` === history.location.pathname) {
+                navItemClasses.push(classes.activeItem);
+            }
+            return (
+                <ListItem
+                    key={category.name}
+                    button onClick={() => this.handleNavClick(category.path)}
+                    className={navItemClasses.join(' ')}>
+                    <ListItemText
+                        inset primary={category.name}
+                        className={classes.ListItem} disableTypography/>
+                </ListItem>
+            );
+        });
+        const postList = posts.map(post => (
+            <TableRow key={post.id}>
+                <TableCell>{post.title}</TableCell>
+                <TableCell>{post.body}</TableCell>
+                <TableCell>{post.author}</TableCell>
+                <TableCell>{post.category}</TableCell>
+                <TableCell>{post.voteScore}</TableCell>
+                <TableCell>{post.deleted}</TableCell>
+            </TableRow>
+        ));
+        return (
+            <div className={classes.root}>
+                <AppBar
+                    position="static" color='default'
+                    classes={{
+                        colorDefault: classes.appBar
+                    }}>
+                    <Toolbar>
+                        <IconButton
+                            onClick={this.toggleSideBar}
+                            className={classes.menuButton} color="inherit"
+                            aria-label="Menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            Readable
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+                <nav className={navBarStyling.join(' ')}>
+                    <List
+                        className={classes.navList}
+                        component='nav'>
+                        {categoriesList}
+                    </List>
+                </nav>
+                <div id='main-content'>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Title</TableCell>
+                                <TableCell>Body</TableCell>
+                                <TableCell>Author</TableCell>
+                                <TableCell>Category</TableCell>
+                                <TableCell>Score</TableCell>
+                                <TableCell>Delete</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {postList}
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
+        );
+    }
+}
 
 ButtonAppBar.propTypes = {
     classes: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
 };
 
 export default withStyles(homeStyle)(ButtonAppBar);
--- frontend/src/styles/homeStyles.js
+++ frontend/src/styles/homeStyles.js
@@ -1,4 +1,4 @@
-import {grayColor, white} from './style';
+import {grayColor, white, transition} from './style';
 
 
 const homeStyle = theme => ({
@@ -12,7 +12,51 @@ const homeStyle = theme => ({
     appBar: {
         backgroundColor: grayColor,
         color: white
-    }
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    sideNav: {
+        ...transition,
+        color: white,
+        whiteSpace: 'nowrap',
+        float: 'left',
+        position: 'static',
+        // position: 'fixed',
+        top: '0',
+        left: '0',
+        bottom: '0',
+        zIndex: 99,
+        overflowY: 'auto',
+        height: '100vh',
+        margin: '0 0 0 -245px',
+        width: '245px',
+        background: grayColor,
+        [theme.breakpoints.up('md')]: {
+            position: 'relative',
+        },
+        [theme.breakpoints.down('md')]: {
+            boxShadow: 'none !important',
+        }
+    },
+    sideNavToggle: {
+        margin: '0',
+        boxShadow: '5px 0 20px rgba(0,0,0,.1)',
+    },
+    navList: {
+        padding: '90px 0 20px',
+        color: white
+    },
+    itemGroup: {
+        opacity: '0.6',
+        paddingTop: '9px',
+        paddingBottom: '9px',
+        position: 'relative',
+    },
+    activeItem: {
+        opacity: '1'
+    },
 });
 
 
